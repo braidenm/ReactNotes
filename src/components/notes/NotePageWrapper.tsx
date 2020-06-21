95e9405e3a2af540863f1e99230c21998f54c11a
Braiden Miller <braidenjamesmiller@gmail.com>
2020-06-21 16:26:48 -0600

    got the login working!!!

--- src/components/notes/NotePageWrapper.tsx
+++ src/components/notes/NotePageWrapper.tsx
@@ -10,6 +10,8 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { getAllNotes, insertNote } from '../../actions/noteActions'
 import { GET_NOTES, INSERT_NOTE } from "../../actions/types";
+import {checkLogin, noteUrl, getHttp } from "../../auth/auth"
+import { notDeepStrictEqual } from "assert";
 
 interface noteState {
   allNotes: Note[];
@@ -48,39 +50,62 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
     this.updateNote = this.updateNote.bind(this);
     this.updateState = this.updateState.bind(this);
     this.search = this.search.bind(this);
+    this.showNotesPage = this.showNotesPage.bind(this);
   }
 
   componentDidUpdate(preProps:any){
     if(preProps !== this.props){
       console.log("in the notePageWrapper after update!!!!!!!!!");
+
+      if(this.props.isLoggedIn){
+
+        this.getAllNotes().then((resp) => {
+          let notes: Note[] = resp;
+          this.updateState(notes);
+        });
+      } else{
+        let notes:Note[] = [];
+        this.updateState(notes);
+      }
+
       
     }
   }
 
   componentDidMount() {
-    let userName = "admin";
-    let password = "password";
+    // let userName = "admin";
+    // let password = "password";
     
     // console.log(this.props.stores);
     
-    this.login(userName, password)
-      .then(() => {
-        return this.getAllNotes();
-        // return this.props.getAllNotes();
-      })
-      .then((resp) => {
-        let notes: Note[] = resp;
-        this.updateState(notes);
-      });
+    // this.login(userName, password)
+    //   .then(() => {
+    //     return this.getAllNotes();
+    //     // return this.props.getAllNotes();
+    //   })
+    //   .then((resp) => {
+    //     let notes: Note[] = resp;
+    //     this.updateState(notes);
+    //   });
+
+    if(this.props.isLoggedIn){
+      this.getAllNotes().then((resp) => {
+            let notes: Note[] = resp;
+            this.updateState(notes);
+          });
+    }else{
+      let notes: Note[] = []
+      this.updateState(notes)
+    }
   }
 
   getAllNotes() {
-    if (!this.checkLogin) {
+    if (!this.props.isLoggedIn) {
       console.error("Not logged in or something. cant get all notes");
 
       // this.router.navigateByUrl('login');
     } else {
-      return axios.get<Note[]>(this.noteUrl, this.getHttp()).then((resp) => {
+      return axios.get<Note[]>(noteUrl, getHttp()).then((resp) => {
         let notes = resp.data.sort((a, b) => {
           if (a.updated > b.updated) {
             return -1;
@@ -94,7 +119,7 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
   }
 
   search(search: string) {
-    if (!this.checkLogin) {
+    if (!this.props.isLoggedIn) {
       console.error("Not logged in or something. cant get all notes");
       // this.router.navigateByUrl('login');
     } else {
@@ -105,7 +130,7 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
         });
       }else {
 
-        axios.get<Note[]>(this.noteUrl + "/search/" + search, this.getHttp()).then(response => {
+        axios.get<Note[]>(noteUrl + "/search/" + search, getHttp()).then(response => {
             let notes = response.data.sort((a, b) => {
               if (a.updated > b.updated) {
                 return -1;
@@ -137,19 +162,19 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
     let notes = this.state.allNotes.slice();
 
     if (note.deleted) {
-      axios.delete(this.noteUrl + "/" + note.id, this.getHttp()).then(() => {
+      axios.delete(noteUrl + "/" + note.id, getHttp()).then(() => {
         notes.splice(index, 1);
         this.updateState(sortNotes(notes));
       });
     } else if (index === -1) {
-      axios.post(this.noteUrl, note, this.getHttp()).then((resp) => {
+      axios.post(noteUrl, note, getHttp()).then((resp) => {
         notes.unshift(resp.data);
         this.updateState(sortNotes(notes));
       });
       // this.props.insertNote(note);
     } else {
       axios
-        .put(this.noteUrl + "/" + note.id, note, this.getHttp())
+        .put(noteUrl + "/" + note.id, note, getHttp())
         .then((resp) => {
           notes[index] = resp.data;
 
@@ -165,11 +190,9 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
     });
   }
 
-  
-
-  render() {
-    return (
-      <div style={{ marginTop: "5%", marginBottom: "3%" }}>
+  showNotesPage(state: any){
+    if(this.props.isLoggedIn){
+      return (<div>
         <div className="row">
           <NoteCounter count={this.state.noteCount}></NoteCounter>
           <Search search={this.search}></Search>
@@ -199,90 +222,103 @@ export default class NotePageWrapper extends Component<NoteProps, noteState> {
         <div className="row">
           <AddNote updateNote={this.updateNote}></AddNote>
         </div>
+      </div>)
+    } else{
+      return (<h1>PLEASE LOGIN</h1>)
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div style={{ marginTop: "5%", marginBottom: "3%" }}>
+        {this.showNotesPage(this.state)}
       </div>
     );
   }
+}
 
   //*****************
   //Auth Stuff
   //******************
 
-  // private baseUrl = environment.baseUrl;
-  private baseUrl = "http://localhost:8085/";
-  private noteUrl = this.baseUrl + "api/notes";
+//   // private baseUrl = environment.baseUrl;
+//   private baseUrl = "http://localhost:8085/";
+//   private noteUrl = this.baseUrl + "api/notes";
 
-  // P U B L I C  M E T H O D S
+//   // P U B L I C  M E T H O D S
 
-  login(username: any, password: any) {
-    // Make credentials
-    const credentials = this.generateBasicAuthCredentials(username, password);
-    // Send credentials as Authorization header (this is spring security convention for basic auth)
-    const httpOptions = {
-      headers: {
-        Authorization: `Basic ${credentials}`,
-        "X-Requested-With": "XMLHttpRequest",
-      },
-    };
-    // create request to authenticate credentials
-    return axios.get<User>(this.baseUrl + "authenticate", httpOptions)
-      .then((res) => {
-        localStorage.setItem("credentials", credentials);
-        return res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+//   login(username: any, password: any) {
+//     // Make credentials
+//     const credentials = this.generateBasicAuthCredentials(username, password);
+//     // Send credentials as Authorization header (this is spring security convention for basic auth)
+//     const httpOptions = {
+//       headers: {
+//         Authorization: `Basic ${credentials}`,
+//         "X-Requested-With": "XMLHttpRequest",
+//       },
+//     };
+//     // create request to authenticate credentials
+//     return axios.get<User>(this.baseUrl + "authenticate", httpOptions)
+//       .then((res) => {
+//         localStorage.setItem("credentials", credentials);
+//         return res;
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
 
-  updateCredentials(username: string, password: string) {
-    const credentials = this.generateBasicAuthCredentials(username, password);
-    localStorage.setItem("credentials", credentials);
-  }
+//   updateCredentials(username: string, password: string) {
+//     const credentials = this.generateBasicAuthCredentials(username, password);
+//     localStorage.setItem("credentials", credentials);
+//   }
 
-  // register(user: any) {
-  //   // create request to register a new account
-  //   return this.http.post<User>(this.baseUrl + 'register', user)
-  //   .pipe(
-  //       catchError((err: any) => {
-  //         console.log(err);
-  //         return throwError('AuthService.register(): error registering user.');
-  //       })
-  //     );
-  // }
+//   // register(user: any) {
+//   //   // create request to register a new account
+//   //   return this.http.post<User>(this.baseUrl + 'register', user)
+//   //   .pipe(
+//   //       catchError((err: any) => {
+//   //         console.log(err);
+//   //         return throwError('AuthService.register(): error registering user.');
+//   //       })
+//   //     );
+//   // }
 
-  logout() {
-    localStorage.removeItem("credentials");
-  }
+//   logout() {
+//     localStorage.removeItem("credentials");
+//   }
 
-  checkLogin() {
-    if (localStorage.getItem("credentials")) {
-      return true;
-    }
-    return false;
-  }
+//   checkLogin() {
+//     if (localStorage.getItem("credentials")) {
+//       return true;
+//     }
+//     return false;
+//   }
 
-  getCredentials() {
-    return localStorage.getItem("credentials");
-  }
+//   getCredentials() {
+//     return localStorage.getItem("credentials");
+//   }
 
-  // P R I V A T E  M E T H O D S
+//   // P R I V A T E  M E T H O D S
 
-  private generateBasicAuthCredentials(username: string, password: string) {
-    return btoa(`${username}:${password}`);
-  }
+//   private generateBasicAuthCredentials(username: string, password: string) {
+//     return btoa(`${username}:${password}`);
+//   }
 
-  private getHttp() {
-    const credentials = this.getCredentials();
-    return {
-      headers: {
-        Authorization: `Basic ${credentials}`,
-        "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-      },
-    };
+//   private getHttp() {
+//     const credentials = this.getCredentials();
+//     return {
+//       headers: {
+//         Authorization: `Basic ${credentials}`,
+//         "Content-Type": "application/json",
+//         "X-Requested-With": "XMLHttpRequest",
+//       },
+//     };
 
-  }
-}
+//   }
+// }
 
 // const mapStateToProps = (state: any)=> ({
 //   allNotes: state.noteReducer.notes,
